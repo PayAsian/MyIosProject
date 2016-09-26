96c03a62beef1b3f3810f0599d0e02d4c5368926
PayAsian <ptre8426@csddocs.org>
2016-09-26 13:35:28 -0600

    i added buttons and color to my project

--- MyIosProject/ViewController.swift
+++ MyIosProject/ViewController.swift
@@ -8,8 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
+    @IBOutlet private weak var textlabel: UITextField!
+    @IBOutlet private weak var button: UIButton!
+    @IBOutlet private weak var slider: UISlider!
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +29,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonClick(sender: UIButton) -> Void
+    {
+        textlabel.text = "You clicked the button!"
+        textlabel.textColor = UIColor.grayColor()
+    }
 
+    private func makeRandomColor() -> UIColor
+    {
+        let randomColor : UIColor
+        
+        //Color in Swift is a precentage - from 0-1 inclusive.
+        //Of type of CGFloat for all components: RGBA
+        
+        let redColor :CGFloat = CGFloat(drand48())
+        let greenColor :CGFloat = CGFloat(Double(arc4random_uniform(256)/255))
+        let blueColor :CGFloat = CGFloat(drand48())
+        let alphaChannel :CGFloat = 1.0
+        
+        randomColor = UIColor(red: redColor, green: greenColor, blue: blueColor, alpha: alphaChannel)
+        
+        return randomColor
+    }
 }
-
